cd63f50978b9e31a3a5ad03e913c95a0ebfe86fa
Atharva-Gaykar <gaykaratharva7@gmail.com>
2025-12-31 20:21:22 +0530

    added app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, UploadFile, File
+from BrainTumorDataCnnModels import detect_and_classify
+
+app = FastAPI(title="Brain Tumor CNN Service")
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    result = detect_and_classify(image_bytes)
+    return result
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8500)
